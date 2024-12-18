3c68a34dd9865101f21260e0388f0afead6e60ce
Muhammed Momen <76847175+MuhammedMomen@users.noreply.github.com>
2024-12-18 20:07:59 +0200

    Delete app_config.py

--- app_config.py
+++
@@ -1,7 +0,0 @@
-# financial_analyzer/config/app_config.py
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-google_api_key = os.getenv("GOOGLE_API_KEY")
-flet_secret_key=os.getenv("FLET_SECRET_KEY")
